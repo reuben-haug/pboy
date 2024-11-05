8c3f752c5c93d50d99e0b37233e325fd697d8e26
Reuben Haug <147293634+reuben-haug@users.noreply.github.com>
2024-11-05 06:41:10 +0000

    Update Dockerfile and devcontainer configuration; adjust conda initialization and update dependencies

--- tests/test_hw.py
+++ tests/test_hw.py
@@ -1,6 +1,6 @@
 from src.main import main
 
 def test_hw(capsys):
-        main.main([])
-        out, err = capsys.readouterr()
-        assert "Hello, World!" in out
+    main.main([])
+    out, err = capsys.readouterr()
+    assert "Hello, World!" in out
